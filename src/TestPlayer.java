cbd9385cd1fac00bc3fdd2a6337092ad7a266caf
EmieD17 <Doucet.emie@gmail.com>
2021-11-15 11:56:55 -0500

    boardTests Done

--- src/TestPlayer.java
+++ src/TestPlayer.java
@@ -3,5 +3,35 @@ import java.lang.reflect.Array;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestPlayer {
-    
+    @Test
+    public void testSetSymbol(){
+        Player p = new Player('X', "test");
+
+        p.SetSymbol('O');
+
+        assertEquals('O', p.GetSymbol());
+    }
+
+    @Test
+    public void testGetSymbol(){
+        Player p = new Player('X', "test");
+
+        assertEquals('X', p.GetSymbol());
+    }
+
+    @Test
+    public void testSetName(){
+        Player p = new Player('X', "test");
+
+        p.SetName("Test2");
+
+        assertEquals("Test2", p.GetName());
+    }
+
+    @Test
+    public void testGetName(){
+        Player p = new Player('X', "test");
+
+        assertEquals("test", p.GetName());
+    }
 }
